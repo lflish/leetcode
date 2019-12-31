94bf0af4bcdd8def4266acf0455d0ce23853bc72
lflish <hxy.gold@gmail.com>
2019-12-31 23:07:34 +0800

    最初版本

---
+++ C/09PalindromeNumber/09PalindromeNumber.c
@@ -0,0 +1,42 @@
+//
+// Created by lflish on 2019/12/31.
+//
+
+#include <stdbool.h>
+#include <stdio.h>
+
+bool isPalindrome(int x){
+
+    if(x < 0)
+        return false;
+    if(x >= 0 && x <= 9)
+        return true;
+
+    int tmp = x;
+    int num = 0;
+
+    for(; tmp > 0; tmp = tmp / 10){
+        num *= 10;
+        num += tmp % 10;
+    }
+
+    if(x == num)
+        return true;
+
+    return false;
+}
+
+int arr[10] = {12345, 12321, 123321, 0, 10, -123};
+
+int main()
+{
+    int i = 0;
+
+    for (i = 0; i < 6; i++){
+        printf("%d\t", arr[i]);
+        printf("%s\n", isPalindrome(arr[i]) == true ? "true" : "false");
+
+    }
+
+    return 0;
+}
