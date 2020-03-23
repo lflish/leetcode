dff6a728a9761cb91009fc28ebe6db16c69af85f
lflish <hxy.gold@gmail.com>
2020-03-23 22:21:30 +0800

    链表的中间结点

---
+++ C/876_MiddleOfTheLinkedList/MiddleOfTheLinkedList.c
@@ -0,0 +1,58 @@
+/*
+Given a non-empty, singly linked list with head node head, return a middle node of linked list.
+
+If there are two middle nodes, return the second middle node.
+
+ 
+
+Example 1:
+
+Input: [1,2,3,4,5]
+Output: Node 3 from this list (Serialization: [3,4,5])
+The returned node has value 3.  (The judge's serialization of this node is [3,4,5]).
+Note that we returned a ListNode object ans, such that:
+ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, and ans.next.next.next = NULL.
+Example 2:
+
+Input: [1,2,3,4,5,6]
+Output: Node 4 from this list (Serialization: [4,5,6])
+Since the list has two middle nodes with values 3 and 4, we return the second one.
+ 
+
+Note:
+
+The number of nodes in the given list will be between 1 and 100.
+*/
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+//@stime 2020 03-23 22:08
+//@endtime 2020 03-23 22:18
+//双百
+#define NULL (void*)0
+
+struct ListNode* middleNode(struct ListNode* head){
+
+    struct ListNode *cur = head; 
+    int num = -1;
+
+    for(; head != NULL; head = head->next){
+
+        if(++num % 2 == 0)
+            continue;
+        cur = cur->next;
+    }
+
+    return cur;
+}
