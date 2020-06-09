df38cebe19d71d8d2a87cb44eb33e0838daffb84
ubuntu20 <hxy.gold@gmail.com>
2020-06-09 23:23:41 +0800

    up list

--- C/SingleList/TwoPtrListt.c
+++ C/SingleList/TwoPtrListt.c
@@ -24,6 +24,7 @@ struct ListNode{
     int val;
     struct ListNode *next;
 };
+//环形链表1
 // 判断是否有 环连表  可以把头的判断直接放到循环里
 bool hasCycle(struct ListNode *head) {
 
@@ -48,6 +49,8 @@ bool hasCycle(struct ListNode *head) {
  *     struct ListNode *next;
  * };
  */
+// 环形链表2
+// 给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null
 struct ListNode *detectCycle(struct ListNode *head) {
     struct ListNode *slow = head;
     struct ListNode *fast = head;
@@ -73,4 +76,57 @@ struct ListNode *detectCycle(struct ListNode *head) {
         head = head->next;
     }
     return NULL;
+}
+
+//找到两个单链表相交的起始节点。
+struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *headB) {
+
+    if(!headA || !headB)
+        return NULL;
+
+    struct ListNode *nodeA = headA;
+    struct ListNode *nodeB = headB;
+
+    int flagA = true;
+    int flagB = true;
+
+    /* find A - B len */
+    while(nodeA && nodeB ){
+
+        if(nodeA == nodeB )        
+            return nodeA;
+
+        nodeA = nodeA->next;
+        if(flagA && nodeA == NULL){
+            nodeA = headB;
+            flagA = false;
+        }
+
+        nodeB = nodeB->next;
+        if(flagB && nodeB == NULL){
+            nodeB = headA;
+            flagB = false;
+        }
+    }
+
+    return NULL;
+}
+
+//给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
+struct ListNode* removeNthFromEnd(struct ListNode* head, int n){
+
+    struct ListNode *fast = head;
+    struct ListNode **slow = &head;
+
+    while(n--)
+        fast = fast->next;
+
+    while(fast){
+        fast = fast->next;
+        slow = &(*slow)->next;
+    }
+
+    *slow = (*slow)->next;
+
+    return head;
 }
